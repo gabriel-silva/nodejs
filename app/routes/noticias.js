c5ff9a61b5e585f9776ba5abad8b0b7020365dcb
Gabriel <gabriel-silva_2011@hotmail.com>
2018-10-17 11:09:25 -0300

    correções na conexão com o banco de dados mysql com MAMP

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -2,7 +2,6 @@ module.exports = function (app) {
     app.get('/noticias', function (req, res) {
 
         var mysql = require('mysql');
-
         var connection = mysql.createConnection({
             host: 'localhost',
             user: 'root',
@@ -20,7 +19,6 @@ module.exports = function (app) {
         });
 
         connection.end();
-
         //res.render('noticias/noticias');
     });
 }
