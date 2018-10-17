9dc6a3985c515361576b86f940b1a1dc4e475d6e
Gabriel <gabriel-silva_2011@hotmail.com>
2018-10-17 11:05:39 -0300

    correções na conexão com o banco de dados mysql com MAMP

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,18 +1,25 @@
-module.exports = function(app){
-    app.get('/noticias', function(req,res){
+module.exports = function (app) {
+    app.get('/noticias', function (req, res) {
 
         var mysql = require('mysql');
 
         var connection = mysql.createConnection({
             host: 'localhost',
             user: 'root',
-            password: '',
-            database: 'portal_noticias'
+            password: 'root',
+            database: 'portal_noticias',
+            socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
         });
+        connection.connect();
 
-        connection.query("SELECT * FROM noticias", function(error, result){
+        connection.query("select * from noticias;", function (error, result) {
+            if(error){
+                console.error("error connecting: "+error.stack);
+            }
             res.send(result);
         });
+
+        connection.end();
 
         //res.render('noticias/noticias');
     });
