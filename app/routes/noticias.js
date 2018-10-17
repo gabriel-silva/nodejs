298b7e4c1b6a294659dceb495a83d779bd266779
Gabriel <gabriel-silva_2011@hotmail.com>
2018-10-17 09:29:20 -0300

    inital commit

---
+++ app/routes/noticias.js
@@ -0,0 +1,19 @@
+module.exports = function(app){
+    app.get('/noticias', function(req,res){
+
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'portal_noticias'
+        });
+
+        connection.query("SELECT * FROM noticias", function(error, result){
+            res.send(result);
+        });
+
+        //res.render('noticias/noticias');
+    });
+}
