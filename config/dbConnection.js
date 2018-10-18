b5fe4d82282e3ef3508236f3d00e6d233c8850de
Gabriel <gabriel-silva_2011@hotmail.com>
2018-10-18 10:16:03 -0300

    correções no código

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,15 +1,16 @@
 var mysql = require('mysql');
 
-var connMySQL = function () {
-    return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'portal_noticias',
-        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-    });
+var connMySQL = function(){
+	console.log('Conexao com bd foi estabelecida');
+	return mysql.createConnection({
+		host : 'localhost',
+		user : 'root',
+		password : '1234',
+		database : 'portal_noticias'
+	});
 }
 
 module.exports = function () {
-    return connMySQL;
+	console.log('O autoload carregou o módulo de conexão com bd');
+	return connMySQL;
 }
